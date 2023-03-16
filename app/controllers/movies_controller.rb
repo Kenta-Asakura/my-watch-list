43e215aefaffd074eb50ec860909245ae21b9614
kenta asakura <asakura.kenta963420@gmail.com>
2023-03-16 16:53:47 +0900

    movies controller added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,10 @@
 class MoviesController < ApplicationController
+  def show
+    @movie = Movie.find(params[:id])
+    authorize @movie
+  end
+
+  def index
+    @movies = policy_scope(Movie)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :movies
   devise_for :users
   root to: 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:create, :new]
+    resources :bookmarks, only: [:index, :create, :new, :destroy]
   # bookmark needs a list
   # no need to nest destroy, we dont need a list
   end
+  resources :movies
   resources :bookmarks, only: [:destroy]
 end
 
