6e11379aed7930a503b0dc90ca47ba8cea52eb40
kenta asakura <asakura.kenta963420@gmail.com>
2023-04-05 21:33:09 +0900

    bookmark model dependecy delete added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
 
   validates :title, :overview, presence: true, uniqueness: true
 end
--- db/migrate/20230216034002_create_bookmarks.rb
+++ db/migrate/20230216034002_create_bookmarks.rb
@@ -4,7 +4,7 @@ class CreateBookmarks < ActiveRecord::Migration[7.0]
       t.string :comment
       t.references :list, null: false, foreign_key: true
       t.references :movie, null: false, foreign_key: true
-      
+
       t.timestamps
     end
   end
