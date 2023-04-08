94629996b79e6f183db965bb31e30466e4023769
kenta asakura <asakura.kenta963420@gmail.com>
2023-04-08 22:28:03 +0900

    style on all pages polished

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -1,15 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="search-movies"
-export default class extends Controller {
-  static targets = ['form']
+// // Connects to data-controller="search-movies"
+// export default class extends Controller {
+//   static targets = ['form']
 
-  connect() {
-    console.log(this.formTarget);
-    console.log('hello');
-  }
+//   connect() {
+//     console.log(this.formTarget);
+//     console.log('hello');
+//   }
 
-  update() {
-    console.log("ajax test");
-  }
-}
+//   update() {
+//     console.log("ajax test");
+//   }
+// }
