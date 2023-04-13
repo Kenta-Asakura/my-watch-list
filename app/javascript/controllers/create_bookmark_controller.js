3eff72d90c0f1a1f80b3320a7cf82728c26ad2a8
kenta asakura <asakura.kenta963420@gmail.com>
2023-04-13 21:49:46 +0900

    ajax testing

--- app/javascript/controllers/create_bookmark_controller.js
+++ app/javascript/controllers/create_bookmark_controller.js
@@ -1,23 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
+  static targets = ['form']
+  
   connect() {
     console.log('hello from create');
   }
 
+  submitForm() {
+    const forms = document.querySelectorAll(".bookmark-btn")
+    forms.forEach(form => {
+      form.preventDefault();
+    });
+  }
+
   // submitForm() {
-  //     btn.addEventListener("click", (e) => {
+    //     btn.addEventListener("click", (e) => {
   //       e.preventDefault();
   //   });
   // }
 
-  submitForm() {
-    const forms = document.querySelectorAll("bookmark-btn")
-    forms.forEach(form=> {
-      form.addEventListener("click", (e) => {
-              e.preventDefault();
-            });
-    });
-  }
+  // submitForm() {
+  //   const forms = document.querySelectorAll(".bookmark-btn")
+  //   forms.forEach(form=> {
+  //     form.addEventListener("click", (e) => {
+  //             e.preventDefault();
+  //           });
+  //   });
+  // }
 }
