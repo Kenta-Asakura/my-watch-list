5980a780fc5eda9b966b7085b04e5a31b36337e2
kenta asakura <asakura.kenta963420@gmail.com>
2023-05-10 21:33:11 +0900

    movies pages (upcoming, nowplaying, toprated) views file partials added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,31 +2,32 @@ class BookmarksController < ApplicationController
   before_action :set_movie, only: [:create]
 
   def index
-    # @movie = Movie.find(params[:movie_id])
     @bookmarks = policy_scope(Bookmark)
-    # @bookmarks = current_user.bookmarks
   end
 
   def create
     @movie = Movie.find(params[:movie_id])
     @bookmark = Bookmark.new(user: current_user, movie: @movie)
     authorize @bookmark
-    @bookmark.save
 
+    @bookmark.save
     @bookmark = Bookmark.find(params[:id])
     if @bookmark
-      @bookmark.destroy
+      # @bookmark.destroy
     # else
     #   @bookmark.save
-    #   flash[:notice] = 'Bookmark was successfully created.'
+      return flash[:notice] = 'Bookmark was successfully created.'
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     authorize @bookmark
-    @bookmark.destroy
-    flash[:notice] = 'Bookmark was successfully deleted.'
+
+    if @bookmark
+      @bookmark.destroy
+      flash[:notice] = 'Bookmark was successfully deleted.'
+    end
   end
 
   private
