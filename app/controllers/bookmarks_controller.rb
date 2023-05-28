97dc9b3389f42a1f0f4a559c82626732058e4b64
kenta asakura <asakura.kenta963420@gmail.com>
2023-05-28 20:29:09 +0900

    commented out ajax settings

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,14 +10,30 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(user: current_user, movie: @movie)
     authorize @bookmark
 
-    respond_to do |format|
-      if @bookmark.save
-        format.html { redirect_to request.referer, notice: 'Bookmark was successfully created.' }
-        format.json { render :create, status: :created }  # Render the create.js.erb template without redirecting
-      else
-        format.html { redirect_to request.referer, alert: 'Failed to create bookmark.' }
-        format.json { render :create, status: :unprocessable_entity }  # Render the create.js.erb template without redirecting
-      end
+    if @bookmark.save
+      redirect_to movies_path, notice: 'Bookmark was successfully created.'
+    else
+      redirect_to movies_path, alert: 'Failed to create bookmark.'
+    end
+    # respond_to do |format|
+    #   if @bookmark.save
+    #     format.html { redirect_to request.referer, notice: 'Bookmark was successfully created.' }
+    #     format.json { render :create, status: :created }  # Render the create.js.erb template without redirecting
+    #   else
+    #     format.html { redirect_to request.referer, alert: 'Failed to create bookmark.' }
+    #     format.json { render :create, status: :unprocessable_entity }  # Render the create.js.erb template without redirecting
+    #   end
+    # end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    authorize @bookmark
+
+    if @bookmark.destroy
+      redirect_to movies_path, notice: 'Bookmark was successfully deleted.'
+    else
+      redirect_to movies_path, alert: 'Failed to delete bookmark.'
     end
   end
 
@@ -25,27 +41,16 @@ class BookmarksController < ApplicationController
   #   @bookmark = Bookmark.find(params[:id])
   #   authorize @bookmark
 
-  #   if @bookmark.destroy
-  #     redirect_to movies_path, notice: 'Bookmark was successfully deleted.'
-  #   else
-  #     redirect_to movies_path, alert: 'Failed to delete bookmark.'
+  #   respond_to do |format|
+  #     if @bookmark.destroy
+  #       format.html { redirect_to movies_path, notice: 'Bookmark was successfully deleted.' }
+  #       format.js   # This will render destroy.js.erb
+  #     else
+  #       format.html { redirect_to movies_path, alert: 'Failed to delete bookmark.' }
+  #       format.js   # This will render destroy.js.erb
+  #     end
   #   end
   # end
-
-  def destroy
-    @bookmark = Bookmark.find(params[:id])
-    authorize @bookmark
-
-    respond_to do |format|
-      if @bookmark.destroy
-        format.html { redirect_to movies_path, notice: 'Bookmark was successfully deleted.' }
-        format.js   # This will render destroy.js.erb
-      else
-        format.html { redirect_to movies_path, alert: 'Failed to delete bookmark.' }
-        format.js   # This will render destroy.js.erb
-      end
-    end
-  end
 
   private
 
--- app/javascript/controllers/create_bookmark_controller.js
+++ app/javascript/controllers/create_bookmark_controller.js
@@ -1,26 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="create-bookmark"
-export default class extends Controller {
-  connect() {
-    console.log('HELLO!!!!');
-    console.log(this.element);
-    console.log(this.itemsTarget)
-    console.log(this.formTarget)
-  }
+// // Connects to data-controller="create-bookmark"
+// export default class extends Controller {
+//   static targets = ["bookmark"]
 
-  send(event) {
-    event.preventDefault();
+//   connect() {
+//     // console.log('HELLO!!!!');
+//     console.log(this.element);
+//     console.log(this.itemsTarget)
+//     console.log(this.formTarget)
+//   }
 
-    console.log('AJAX request sent');
-    fetch(this.formTarget.action, {
-      method: "POST",
-      headers: { "Accept": "application/json" },
-      body: new FormData(this.formTarget)
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-      })
-  }
-}
+//   send(event) {
+//     event.preventDefault();
+
+//     console.log('AJAX request sent');
+//     fetch(this.formTarget.action, {
+//       method: "POST",
+//       headers: { "Accept": "application/json" },
+//       body: new FormData(this.formTarget)
+//     })
+//       .then(response => response.json())
+//       .then((data) => {
+//         console.log(data)
+//       })
+//   }
+// }
